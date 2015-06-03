020b8a1bcfe41c34068c7073df90481f12b149ac
jon536 <jon-536@mail.ru>
2015-06-03 12:59:53 +0300

    asdf

--- java-advanced-2015_/java/info/kgeorgiy/java/advanced/hello/HelloClient.java
+++
@@ -1,8 +0,0 @@
-package info.kgeorgiy.java.advanced.hello;
-
-/**
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
-public interface HelloClient {
-    void start(String host, int port, String prefix, int requests, int threads);
-}
